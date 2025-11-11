f2d5bea4f3ae62de5fe1bb9ae86233d0119805dd
JopraZ <louisdemol9@gmail.com>
2025-11-11 16:33:20 +0100

    touche entrer pour ajouter taches

--- script.js
+++ script.js
@@ -1,27 +1,43 @@
 let taskInput = document.getElementById("taskInput");
 let button = document.getElementById("button");
 let list = document.getElementById("list");
-let search = document.getElementById("search");
 
-button.addEventListener("click", function() {
+function ajouterUneTache() {
     let task = taskInput.value.trim();
     if (task !== "") {
+        // Créer l'élément li
         let listItem = document.createElement("li");
         listItem.textContent = task;
-        list.appendChild(listItem);
-        taskInput.value = "";
+
+        // Listener pour cocher/décocher
+        listItem.addEventListener("click", function() {
+            listItem.classList.toggle("completed");
+        });
+
+        // Ajouter le bouton supprimer
         let deleteButton = document.createElement("button");
         deleteButton.textContent = "supprimer";
-        listItem.appendChild(deleteButton);
-        deleteButton.addEventListener("click", function(){
+
+        // Supprimer sans déclencher le toggle
+        deleteButton.addEventListener("click", function(e){
+            e.stopPropagation(); // empêche le clic sur li
             list.removeChild(listItem);
-        })
+        });
+
+        listItem.appendChild(deleteButton);
+        list.appendChild(listItem);
+
+        // Réinitialiser l'input
+        taskInput.value = "";
     }
-});
-taskInput.addEventListener("keydown", function(event) {
-    if (event.key === "Enter") {
-        button.click();
-        event.prevenetDefault();
-    }
+};
+
+button.addEventListener("click", ajouterUneTache);
+
+taskInput.addEventListener("keydown", function(e) {
+    if (e.key === "Enter") {
+        e.preventDefault();
+        ajouterUneTache();
+        }
 });
 
